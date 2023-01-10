fde15b98b241022dca9b4cb1083e673dcbe853d0
Nemanja <mneca99+git@gmail.com>
2023-01-10 18:38:51 +0100

    first commit

---
+++  cron.py
@@ -0,0 +1,6 @@
+from app import getScreenshot
+from time import sleep
+
+while True:
+    getScreenshot()
+    sleep(1)
---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+from flask import request
+import os
+import hashlib
+import validators
+
+location = "/var/www/html/"
+location = ""
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    content = ""
+    domain = request.args.get('domain', default = "", type = str)
+    if domain != '':
+        if validators.url(domain):
+            name = randName()
+            f = open(location + 'tasks/' + name, "a")
+            f.write(domain)
+            f.close()
+
+            waiting = 0
+            image = location + "static/images/" + name + ".png"
+
+            link = "/static/images/" + name + ".png"
+            content = "<a href='" + link + "' target='_blank'>" + link + "<br>"
+            content += "<img src='" + link + "' width='300px'></a><br>"
+            while not os.path.exists(image):
+                waiting += 1
+                sleep(1)
+                if waiting > 15:
+                    content = "Unexpected error<br>"
+        else:
+            content = "Not valid domain"
+
+    return content + '<hr><form><label>Type website:</label><input type="text" name="domain" placeholder="domain"><input type="submit"></form>'
+
+def randName():
+    return hashlib.sha256(os.urandom(64)).hexdigest()
+
+def screenshot(domain, task):
+    options = Options()
+    options.add_argument('--headless')
+
+    driver = webdriver.Firefox(options=options)
+    driver.get(domain)
+    sleep(1)
+
+    image = task + ".png"
+
+    driver.get_screenshot_as_file(location + "static/images/" + image)
+    driver.quit()
+    return location + "static/images/" + image
+
+def getScreenshot():
+    tasks = os.listdir(location + "tasks")
+
+    for task in tasks:
+        file = open(location + "tasks/" + task, "r")
+        domain = file.read()
+        if domain != '':
+            screenshot(domain, task)
+        os.remove(location + "tasks/"+ task)
+
+if __name__ == '__main__':
+    app.run()
+
+
+# sudo apt install xfce4 xfce4-goodies
+# sudo apt install tightvncserver 
+# sudo apt install dbus-x11
+
+# sudo apt install xfce4 xfce4-goodies tightvncserver dbus-x11 python3 python3-pip firefox-esr apache2
+
+
+# vncserver
+#PORT 5901
+
+        # vncserver -kill :1
+        # mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+        # nano ~/.vnc/xstartup
+            # #!/bin/bash
+            # xrdb $HOME/.Xresources
+            # startxfce4 &
+
+        # sudo chmod +x ~/.vnc/xstartup
+        # vncserver
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-debian-11
